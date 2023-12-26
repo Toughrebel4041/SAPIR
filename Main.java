822a9281f2f8d3362d0bf560c1a5cde989702944
Toughrebel4041 <raihansultanpb@gmail.com>
2023-12-26 17:41:38 +0700

    Menu added

--- Main.java
+++ Main.java
@@ -5,5 +5,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner inputData = new Scanner(System.in);
+        boolean mainMenu = true;
+        System.out.println("\nWelcome to SAPIR");
+
+        do{
+            System.out.println("1. Cari Data Pasien");
+            System.out.println("2. Tambah Data Pasien Baru");
+            System.out.println("3. Hapus Data Pasien");
+            System.out.println("4. Keluar");
+            System.out.println("\nPilihan Anda: ");
+            int menuSelect = input.nextInt();
+            switch (menuSelect) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        } while (mainMenu != false);
     }
 }
